03ac8f46016250b141b2d9ab6d19686e8ccbdedb
Александр <yaroslavskiy2003@list.ru>
2019-06-28 10:38:30 +0200

    Домашка номер 2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 2500000,
+    income = "Фриланс",
+    addExpenses = "Еда, квартира, ипотека",
+    deposit = true,
+    mission = 10000000,
+    period = 4,
+    budgetDay = money / 30;
 
-alert("Hello,my name is Sasha");
-console.log("It's me again");
+console.log(typeof money,typeof income,typeof deposit);
+console.log(income.length);
+console.log(period,mission);
+console.log(addExpenses.toLowerCase().split(", "));
+console.log(budgetDay,money % 30);
