2380752352128b8b9799daecb7b823f07aac4b76
Sanek200334 <40474553+Sanek200334@users.noreply.github.com>
2019-07-03 22:26:47 +0300

    Update script.js

--- diffTask/script.js
+++ diffTask/script.js
@@ -1,5 +1,11 @@
 let num = 266219,
-    numP = 2 * 6 * 6 * 2 * 1 * 9;
+    numP = 1;
+
+num = num.toString();
+
+for (var i = 0; i < num.length; i++) {
+  numP *= num[i];
+}
 
 console.log(numP);
 console.log((numP**3).toString().slice(0,2));
