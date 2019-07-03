bcaa27a2ee73d1f191b9528caa595ed99ca287b2
Александр <yaroslavskiy2003@list.ru>
2019-07-03 23:11:00 +0300

    Обязательная домашка к 5 уроку

--- script.js
+++ script.js
@@ -1,53 +1,91 @@
 'use strict';
 
 let mission = 1000000,
+    money,
     income = "Фриланс",
-    money = prompt("Ваш месячный доход?"),
     addExpences = prompt("Перечислите возможные расходы за рассчитываемый период через запятую").split(','),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     period = 3;
 
-let showTypeOf = function(item){
-  console.log(item,typeof item);
+let start = function(){    //Функция валидации ввода дохода
+  do{
+    money = prompt("Ваш месячный доход?");
+  }while(isNaN(money) || money === "" || money === null);
 }
 
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-let monthLose = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    monthAmount = +prompt("Во сколько это обойдется?");
-let monthLose1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    monthAmount1 = +prompt("Во сколько это обойдется?");
-
-let budgetMonth = money - (monthAmount + monthAmount1);
-let budgetDay = Math.floor(budgetMonth / 30);
+start();
 
 
-function getStatusIncome(){
-    if (budgetDay > 800) {
+let monthLose1,
+    monthLose2;
+
+let expencesMonth = function(){ // Функция получения уровня затрат в месяц
+  let sum = 0,promptSum;
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      do{
+        monthLose1 = prompt("Какие обязательные ежемесячные расходы у вас есть?1");
+      }while(monthLose1 === "" || monthLose1 === null);
+      while(isNaN(promptSum) || promptSum === "" || promptSum === null){
+          promptSum = +prompt("Во сколько это обойдется?",2500);
+          sum+= promptSum;
+      }
+      promptSum = null;
+    }else if(i === 1){
+      do{
+          monthLose2 = prompt("Какие обязательные ежемесячные расходы у вас есть?2");
+        }while(monthLose2 === "" || monthLose2 === null);
+      while(isNaN(promptSum) || promptSum === "" || promptSum === null){
+          promptSum = +prompt("Во сколько это обойдется?",2500);
+          sum+= promptSum;
+        }
+      }
+    }
+  return sum;
+}
+
+let expencesAmount = expencesMonth();
+
+let accumulatedMonth =  function getAccumulatedMonth(){ //Функция возвращающая прибыль в месяц
+  if ((money - expencesAmount) < 0) {
+    return 0;
+  }else{
+    return money - expencesAmount;
+  }
+}
+
+let budgetDay = function(){//Функция возвращающая дневную прибыль
+  if (accumulatedMonth() === 0) {
+    return -1;
+  }else{
+    return Math.floor(accumulatedMonth() / 30);
+  }
+};
+
+
+function getStatusIncome(){//Функция определяющая уровень дохода
+    if (budgetDay() > 800) {
        return('Высокий уровень дохода');
-    }else if(budgetDay > 300 && budgetDay <= 800){
+    }else if(budgetDay() > 300 && budgetDay() <= 800){
         return('Средний уровень дохода');
-    }else if(budgetDay <= 300 && budgetDay >= 0){
+    }else if(budgetDay() <= 300 && budgetDay() >= 0){
       return('Низкий уровень дохода');
     }else{
       return('Что-то пошло не так');
     }
 }
 
+console.log(getStatusIncome());
 
-function getExpensesMonth(){
-  return monthAmount + monthAmount1
+
+function getTargetMonth(){//Функция возвращающая число ммесяцев через которое цель будет достигнута
+  let targetMonth = Math.floor(mission / accumulatedMonth());
+  if (targetMonth < 0 || isNaN(targetMonth) || targetMonth === Infinity) {
+    return "Цель не будет достигнута";
+  }else{
+    return "You'll reach your target in " + targetMonth + " months";
+  }
 }
 
-let accumulatedMonth =  function getAccumulatedMonth(){
-  return money - (monthAmount + monthAmount1)
-}
-
-function getTargetMonth(){
-  return Math.floor(mission/budgetMonth)
-}
-
-console.log("You'll reach your target in " + getTargetMonth() + " months");
-console.log("За " + period + " месяца вы накопите - " + budgetMonth * period + " деняк");
+console.log(getTargetMonth());
+console.log("За " + period + " месяца вы накопите - " + accumulatedMonth() * period + " деняк");
