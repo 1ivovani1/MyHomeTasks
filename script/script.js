a5a17bbb48264b8d9a324073ea55a9aed293d798
Александр <yaroslavskiy2003@list.ru>
2019-07-20 23:24:50 +0200

    Обязательная домашка к 13 уроку

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
 window.addEventListener('DOMContentLoaded',function() {
     'use strict';
-    const months = ['january','february','march','april','may','june','july','august','september','october','november','december'];
 
+    //Таймер
+    const months = ['january','february','march','april','may','june','july','august','september','october','november','december'];
     let dateNow = new Date(),
         nextDay = `${dateNow.getUTCDate() + 1} ${months[dateNow.getUTCMonth()]} ${dateNow.getUTCFullYear()}`;
-
-
-    function countTimer(deadline){
+    const countTimer = (deadline) => {
        let dateNow1 = new Date(),
            nextDay1 = `${dateNow1.getUTCDate() + 2} ${months[dateNow1.getUTCMonth()]} ${dateNow1.getUTCFullYear()}`;
       let timerHours = document.querySelector('#timer-hours'),
@@ -37,7 +36,65 @@ window.addEventListener('DOMContentLoaded',function() {
         timerSeconds.textContent = timer.seconds;
 
     }
-
     let timerInterval = setInterval(countTimer,1000,nextDay);
+
+    //Меню
+    const toggleMenu = () => {
+      const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+      const handlerMenu = () => {
+        menu.classList.toggle('active-menu')
+      }
+
+      btnMenu.addEventListener('click',handlerMenu);
+      closeBtn.addEventListener('click',handlerMenu);
+      menuItems.forEach((item) => item.addEventListener('click',handlerMenu));
+    }
+    toggleMenu();
+
+    // Поп-ап окно
+    const togglePopup = () => {
+      const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+      let count = 0,
+          animInterval;
+
+
+
+      popupBtn.forEach((item) => {
+          item.addEventListener('click', () => {
+            if (window.innerWidth > 1350) {
+              popupContent.style = `position:absolute;top:0`
+              popup.style.display = `block`;
+              animInterval = requestAnimationFrame(popupAnim);
+            }else{
+              popup.style.display = `block`;
+            }
+        })
+      });
+      popupClose.addEventListener('click', () => {
+        popup.style.display = `none`;
+        togglePopup();
+      });
+
+      const popupAnim = () => {
+        let height = popupContent.getBoundingClientRect().height;
+        animInterval = requestAnimationFrame(popupAnim);
+        count += 1.5;
+        if ((window.innerHeight / 2 - height / 2) > count) {
+          popupContent.style.top = `${count}px`;
+        }else{
+          cancelAnimationFrame(popupAnim);
+        }
+      }
+
+    }
+    togglePopup();
 
 });
