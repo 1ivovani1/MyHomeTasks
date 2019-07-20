b910b20283a1fdfc9938a0cd59470a4e44e927bf
Александр <yaroslavskiy2003@list.ru>
2019-07-21 00:54:40 +0200

    Усложненная домашка к 13 уроку

--- script/script.js
+++ script/script.js
@@ -38,12 +38,32 @@ window.addEventListener('DOMContentLoaded',function() {
     }
     let timerInterval = setInterval(countTimer,1000,nextDay);
 
+    //Функция прокрутки до определенного элемента
+    const slowScroll = (id) => {
+        const item = document.querySelector(id),
+              topMarg = item.getBoundingClientRect().top;
+
+
+        let scrollAnim = setInterval(() => {
+              if (topMarg > document.documentElement.scrollTop) {
+                if (topMarg >= 1500) {
+                  document.documentElement.scrollTop += 8;
+                }else{
+                  document.documentElement.scrollTop += 4;
+                }
+              }else{
+                clearInterval(scrollAnim);
+              }
+        },1)
+    }
+
     //Меню
     const toggleMenu = () => {
       const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+            menuItems = menu.querySelectorAll('ul>li'),
+            nextPage = document.querySelector('.next-part');
 
       const handlerMenu = () => {
         menu.classList.toggle('active-menu')
@@ -51,7 +71,34 @@ window.addEventListener('DOMContentLoaded',function() {
 
       btnMenu.addEventListener('click',handlerMenu);
       closeBtn.addEventListener('click',handlerMenu);
-      menuItems.forEach((item) => item.addEventListener('click',handlerMenu));
+      menuItems.forEach((item,index) => item.addEventListener('click',(e) => {
+          e.preventDefault();
+          handlerMenu();
+          switch (index) {
+            case 0:
+              slowScroll("#service-block");
+              break;
+            case 1:
+              slowScroll('#portfolio');
+              break;
+            case 2:
+              slowScroll('#calc');
+              break;
+            case 3:
+              slowScroll('#command');
+              break;
+            case 4:
+              slowScroll('#connect');
+
+              break;
+          }
+
+      }));
+
+      nextPage.addEventListener('click',(e) => {
+        e.preventDefault();
+        slowScroll('#service-block');
+      })
     }
     toggleMenu();
 
@@ -96,5 +143,7 @@ window.addEventListener('DOMContentLoaded',function() {
 
     }
     togglePopup();
+
+    
 
 });
